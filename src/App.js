87171186ddd8efc6f9fa10ea872d7611917cbb43
NHARISHA <nharisha552@gmail.com>
2021-07-28 12:22:49 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let App = () => {
+    const [balance, setBalance] = useState(550);
+    const [value, setValue] = useState(Number);
+    const [Transaction, setTransaction] = useState([]);
+
+    const onChangeHandler = (event) => {
+        setValue(event.target.value)
+        event.preventDefault();
+    }
+
+    const addValue = () => {
+        setBalance(`${parseInt(balance) + parseInt(value)}`)
+        
+        setTransaction(Transaction => ([...Transaction, `${new Date().toLocaleTimeString()} ${value} add`]));
+    }
+
+    const removeValue = () => {
+        setBalance(balance - value)
+        setTransaction(Transaction => ([...Transaction, `${new Date().toLocaleTimeString()} ${value} remove`]));
+    }
+    return (
+        <div>
+{/* <pre>{JSON.stringify(balance)}</pre>
+
+    <pre>{JSON.stringify(Transaction)}</pre> */}
+    <h1>Balance : {balance}</h1>
+            <input type="number" placeholder="Enter money to add or remove " onChange={onChangeHandler} ></input>
+            <button type="button" onClick={addValue} >Add</button>
+            <button type="button" onClick={removeValue}>Remove</button>
+            <br></br>
+            <div>
+              <ul>
+                 
+                {
+                  Transaction.map((trans)=>{
+                    return <li> { trans}</li>
+                  })
+                }
+               {/*  <h3>Transactions:{Transaction}</h3> */}
+                </ul>
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
